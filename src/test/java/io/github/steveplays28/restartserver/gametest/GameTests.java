c0baaa58fe9508ac19c88c35fb284c7efe240d0a
truman <mail@trumanmiller.com>
2025-11-21 01:03:44 -0600

    update for 1.21.6

--- src/test/java/io/github/steveplays28/restartserver/gametest/GameTests.java
+++ src/test/java/io/github/steveplays28/restartserver/gametest/GameTests.java
@@ -2,18 +2,17 @@ package io.github.steveplays28.restartserver.gametest;
 
 import io.github.steveplays28.restartserver.RestartServer;
 import io.github.steveplays28.restartserver.commands.RestartCommand;
-import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.server.command.CommandOutput;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 
 public class GameTests {
-	@GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, batchId = "test_restart_command")
+	@GameTest
 	public void testRestartCommand(TestContext context) {
 		var world = context.getWorld();
 		var server = context.getWorld().getServer();
